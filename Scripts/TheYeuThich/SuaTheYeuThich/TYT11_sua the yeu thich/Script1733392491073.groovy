a2348004f31e6a8f5f27ace97c175c4d24353ca8
tester <tester.vht@gmail.com>
2025-02-06 17:29:21 +0700

    commit

--- Scripts/TheYeuThich/SuaTheYeuThich/TYT11_sua the yeu thich/Script1733392491073.groovy
+++ Scripts/TheYeuThich/SuaTheYeuThich/TYT11_sua the yeu thich/Script1733392491073.groovy
@@ -17,8 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('TheYeuThich/ThemTheKhiTrangChuDaCoThietBi/TYT10_them the thiet bi'), [:], FailureHandling.STOP_ON_FAILURE)
-
 Mobile.startExistingApplication(GlobalVariable.appID, FailureHandling.STOP_ON_FAILURE)
 
 Mobile.tap(findTestObject('ObjectManHinhChinh/ObjectManTrangChu/ObjectTheYeuThich/btnOption'), 0)
